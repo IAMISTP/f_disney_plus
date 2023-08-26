9a4035218979861c934defd7bdd11b4c710e10c4
박혜민 <hyeminpak@gmail.com>
2023-08-27 03:19:35 +0900

    Axios 인스턴스 생성 및 요청 셋팅

---
+++ src/api/axios.js
@@ -0,0 +1,10 @@
+const { default: axios } = require("axios");
+
+const instance = axios.create({
+  baseURL: "https://api.themoviedb.org/3",
+  params: {
+    api_key: process.env.REACT_APP_API_KEY,
+    language: "ko-KR",
+  },
+});
+export default instance;
