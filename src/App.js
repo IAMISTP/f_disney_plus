2e5c77eb17d49bea4abc875a00d953d714ed6074
박혜민 <hyeminpak@gmail.com>
2023-08-28 00:41:11 +0900

    영화 나열을 위한 Row컴포넌트 생성하기

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import { styled } from "styled-components";
 import Nav from "./components/Nav";
 import Banner from "./components/Banner";
 import Categroy from "./components/Categroy";
+import Row from "./components/Row";
+import requests from "./api/requests";
 
 function App() {
   return (
@@ -10,6 +12,18 @@ function App() {
       <Nav />
       <Banner />
       <Categroy />
+      <Row title="Trending Now" id="TN" fetchUrl={requests.fetchTrending} />
+      <Row title="Top Rated" id="TR" fetchUrl={requests.fetchTopRated} />
+      <Row
+        title="Action Movies"
+        id="AM"
+        fetchUrl={requests.fetchActionMovies}
+      />
+      <Row
+        title="Comedy Movies"
+        id="CM"
+        fetchUrl={requests.fetchComedyMovies}
+      />
     </Container>
   );
 }
