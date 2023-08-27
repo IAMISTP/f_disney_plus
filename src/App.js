f09a13811d2b14bd8a7caac39f37183f753bebcc
박혜민 <hyeminpak@gmail.com>
2023-08-27 17:34:12 +0900

    스타일 컴포넌트를 이용한 비디오 배너 생성하기

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { styled } from "styled-components";
 
 function App() {
   return (
@@ -23,3 +24,22 @@ function App() {
 }
 
 export default App;
+
+const Container = styled.main`
+  position: relative;
+  min-height: calc(100vh - 250px);
+  overflow-x: hidden;
+  display: block;
+  top: 72px;
+  padding: 0 calc(3.5vw + 5px);
+
+  &:after {
+    background: url("/images/home-background.png") center center / cover
+      no-repeat fixed;
+    content: "";
+    position: absolute;
+    inset: 0px;
+    opacity: 1;
+    z-index: -1;
+  }
+`;
