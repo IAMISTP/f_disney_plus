66b5b596243b588c980f555ccd33788da775ecdc
박혜민 <hyeminpak@gmail.com>
2023-08-30 19:35:41 +0900

    useLocation 이용해서 검색페이지 구현 , UI 까지 구현

--- src/App.js
+++ src/App.js
@@ -3,13 +3,14 @@ import "./App.css";
 import Nav from "./components/Nav";
 import LoginPage from "./pages/LoginPage";
 import MainPage from "./pages/MainPage";
-import DetilPage from "./pages/DetilPage";
+import DetailPage from "./pages/DetailPage";
 import SearchPage from "./pages/SearchPage";
 
 const Layout = () => {
   return (
     <div>
       <Nav />
+
       <Outlet />
     </div>
   );
@@ -19,11 +20,12 @@ function App() {
   return (
     <div className="app">
       <Routes>
-        <Route path="/" element={<Layout />} />
-        <Route index element={<LoginPage />} />
-        <Route path="main" element={<MainPage />} />
-        <Route path=":movieId" element={<DetilPage />} />
-        <Route path="search" element={<SearchPage />} />
+        <Route path="/" element={<Layout />}>
+          <Route index element={<LoginPage />} />
+          <Route path="main" element={<MainPage />} />
+          <Route path=":movieId" element={<DetailPage />} />
+          <Route path="search" element={<SearchPage />} />
+        </Route>
       </Routes>
     </div>
   );
